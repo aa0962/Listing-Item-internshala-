06006b6de5bb2e98f43ba5354ffe98dea9939666
Aryan Hedaoo <aryanhedaoo@Aryans-MacBook-Air.local>
2024-08-15 22:58:48 +0530

    final done

--- server/server.js
+++ server/server.js
@@ -10,10 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));
 
